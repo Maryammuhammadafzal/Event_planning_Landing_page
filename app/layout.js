3beb8d887d0e9df73a2df0d8086ca720c73b5fe9
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-28 10:26:27 +0500

    Start creating event planning landing page

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +10,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Hero/>
+      <div className="main">
         {children}
+      </div>
+      <Footer/>
       </body>
     </html>
   );
---
+++ components/PlanningCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlanningCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlanningCard
