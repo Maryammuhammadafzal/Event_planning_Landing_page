d30130433dd7ac02ed89ad27e1057de07cc7dcd2
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-28 13:53:17 +0500

    section 1 complete

--- app/Hero/Page.jsx
+++ app/Hero/Page.jsx
@@ -5,15 +5,15 @@ import PlanningCard from "@/components/PlanningCard";
 import Marquee from "@/components/Marquee";
 const HeroPage = () => {
   return (
-    <div className="w-full h-screen flex flex-col gap-7 justify-center items-center overflow-x-hidden">
+    <div className="w-full h-screen flex flex-col gap-7 justify-center items-center overflow-x-hidden md:mb-16 sm:mb-12 mb-6">
       <div
-        className="relative w-full h-screen bg-center bg-cover bg-no-repeat"
+        className="relative w-full h-screen bg-left-top sm:bg-left-bottom md:bg-center bg-cover bg-no-repeat"
         style={{ backgroundImage: `url(${HeroImage.src})` }}
       >
         <div className="header absolute  top-12 left-0 w-full h-[80px] flex justify-center items-center">
                 <Header/>
         </div>
-        <div className="absolute w-full h-[400px] -bottom-20 md:right-20 flex justify-center md:justify-end items-center z-30">
+        <div className="absolute w-full h-[400px] bottom-0 md:-bottom-20 md:right-20 flex justify-center md:justify-end items-center z-30">
                 <PlanningCard/>
         </div>
       </div>
--- components/PlanningCard.jsx
+++ components/PlanningCard.jsx
@@ -5,15 +5,15 @@ import { Button } from "./ui/button";
 
 const PlanningCard = () => {
   return (
-    <div className="sm:w-[560px] h-[400px] w-[95%] flex flex-col justify-center items-center gap-5 card-bg">
+    <div className="sm:w-[560px] h-[400px] w-[97%] flex flex-col justify-center items-center gap-5 card-bg">
       <div className="logo w-auto h-auto">
         <Image src={Logo2} alt="Logo" className="min-w-[130px]" />
       </div>
-      <div className="content w-full h-auto flex flex-col gap-3 px-6 text-center justify-center items-center text-white">
-        <h1 className="trochut font-bold text-3xl sm:text-4xl md:text-5xl">
+      <div className="content w-full h-auto flex flex-col gap-3 px-3 sm:px-6 text-center justify-center items-center text-white">
+        <h1 className="trochut font-bold text-2xl xs:text-3xl sm:text-4xl md:text-5xl">
           Transforming Your Vision Into Unforgettable Events
         </h1>
-        <p className="sunflower text-base font-light px-20">
+        <p className="sunflower text-sm sm:text-base font-light sm:px-10 md:px-20">
                 From intimate gatherings to grand celebrations, we bring your dreams to life.
         </p>
       </div>
