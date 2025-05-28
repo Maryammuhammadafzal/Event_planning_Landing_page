b233be613cfa6d888e5e7210c59428e49bce687d
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-28 13:03:55 +0500

    making-respnsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,77 @@
 import Image from "next/image";
 import React from "react";
-import Logo from "../public/logo.png"
+import Logo from "../public/logo.png";
 import Link from "next/link";
 import { MdOutlineMenuOpen } from "react-icons/md";
 const Header = () => {
   return (
     <header className="w-[85%] bg-white rounded-full flex justify-center items-center h-[80px]">
-      <div className="w-full h-auto flex items-center justify-between sm:px-6 md:px-7 lg:px-15">
+      <div className="w-full h-auto flex items-center justify-between px-6 md:px-7 lg:px-15">
         <div className="logo w-auto h-auto">
-                <Image 
-                 src={Logo}
-                 alt="Logo"
-                 className="min-w-[130px]"
-                 />
+          <Image src={Logo} alt="Logo" className="max-w-[100px] md:min-w-[130px]" />
         </div>
         <div className="tabs w-auto h-auto flex justify-center items-center ">
-              <ul className="flex justify-center items-center sm:hidden lg:gap-11 md:gap-4 w-auto h-auto sunflower text-secondary xl:pr-10">
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Home</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">About</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Services</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Events</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Portfolio</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Blog</Link></li>
-                <li><Link href="/" className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary">Contact Us</Link></li>
-                </ul>  
-                <div className="menu-icon">
-<MdOutlineMenuOpen size={35}/>
-                </div>
+          <ul className=" justify-center items-center md:flex hidden lg:gap-11 md:gap-4 w-auto h-auto sunflower text-secondary xl:pr-10">
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/"
+                className="hover:text-primary focus:text-primary text-base hover:font-semibold focus-font-semibold active:font-semibold active:text-primary"
+              >
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+          <div className="menu-icon text-secondary md:hidden flex justify-center items-center">
+            <MdOutlineMenuOpen size={35} />
+          </div>
         </div>
       </div>
     </header>
--- components/PlanningCard.jsx
+++ components/PlanningCard.jsx
@@ -5,12 +5,12 @@ import { Button } from "./ui/button";
 
 const PlanningCard = () => {
   return (
-    <div className="w-[560px] h-[400px] flex flex-col justify-center items-center gap-5 card-bg">
+    <div className="sm:w-[560px] h-[400px] w-[95%] flex flex-col justify-center items-center gap-5 card-bg">
       <div className="logo w-auto h-auto">
         <Image src={Logo2} alt="Logo" className="min-w-[130px]" />
       </div>
       <div className="content w-full h-auto flex flex-col gap-3 px-6 text-center justify-center items-center text-white">
-        <h1 className="trochut font-bold text-5xl">
+        <h1 className="trochut font-bold text-3xl sm:text-4xl md:text-5xl">
           Transforming Your Vision Into Unforgettable Events
         </h1>
         <p className="sunflower text-base font-light px-20">
