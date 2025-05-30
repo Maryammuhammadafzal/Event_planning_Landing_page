01c91a3b52bde110365755a84a11f778c9629b2b
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-30 23:57:07 +0500

    adding testimonial Slider

--- components/Testmonial-Slider.jsx
+++ components/Testmonial-Slider.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 
 const TestmonialSlider = () => {
   return (
-    <div>
-      
-    </div>
+     <Carousel setApi={setApi} className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <span className="text-4xl font-semibold">{index + 1}</span>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
   )
 }
 
