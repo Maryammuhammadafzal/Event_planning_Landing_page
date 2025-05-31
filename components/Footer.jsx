13abe2966d24d1286c0e9ffdd2be18ae7d0a9af1
maryammuhammadafzal <natiqnagori7@gmail.com>
2025-05-31 12:14:12 +0500

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -73,7 +73,7 @@ const Footer = () => {
           </div>
         </footer>
         <div className="w-full text-center h-auto justify-center items-center text-secondary text-sm mb-10">
-          © 2025 Haven Planner. All Rights Reserved by <a href="Rootletsolutions.com" className="text-primary">Rootletsolutions.com</a>
+          © 2025 Haven Planner. All Rights Reserved by <a href="https://rootletsolutions.com/" target="_blank" className="text-primary">Rootletsolutions.com</a>
         </div>
       </div>
     </div>
